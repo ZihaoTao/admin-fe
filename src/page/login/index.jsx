bf35df1111a898291d368903caa35262067a3b1b
Zihao Tao <taozihao@uw.edu>
2019-06-27 11:55:56 -0700

    d

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -12,7 +12,7 @@ class Login extends React.Component {
         this.state={
             username: '',
             password: '',
-            redirect: _mm.getUrlParam('redirect') || '/home'
+            redirect: _mm.getUrlParam('redirect') || '/'
         }
     }
     componentWillMount() {
