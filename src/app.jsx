38d401c7f8631eab533675e9cb3bfd5cce5864c3
Zihao Tao <taozihao@uw.edu>
2019-06-27 13:02:15 -0700

    s

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,9 @@
 import React            from 'react';
-import ReactDOM         from 'react-dom';
+import ReactDOM         from 'react-dom'; 
+
+import 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { BrowserRouter as Router, Switch, Redirect, Route, Link } from 'react-router-dom'
 
 import Layout           from 'component/layout/index.jsx';
